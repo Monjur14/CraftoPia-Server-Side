28f5f04fb278002579f92c42a4e69087cd3e2b8a
Monjur Hossen <14monjurhossen@gmail.com>
2024-04-30 16:34:15 +0600

    get mathod complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { ObjectId } = require('mongodb'); 
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,11 +8,6 @@ const port = process.env.PORT || 5000;
 //Middleware
 app.use(cors());
 app.use(express.json());
-
-//USER NAME: 14monjurhossen
-//PASS: 4AM5UJThQp8PckmZ
-//console.log(process.env.DB_USER)
-//console.log(process.env.DB_PASS)
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -27,10 +23,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const craftCollection = client.db("craftDB").collection("item")
-
     app.get("/items", async(req, res) => {
       const cursor = craftCollection.find()
       const result = await cursor.toArray();
@@ -41,6 +36,42 @@ async function run() {
       const newItem = req.body;
       console.log(newItem)
       const result = await craftCollection.insertOne(newItem);
+      res.send(result)
+    })
+
+    app.delete("/items/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await craftCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/items/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await craftCollection.findOne(query)
+      res.send(result);
+    })
+
+    app.put('/items/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true};
+      const updatedItem = req.body;
+      const SingleItem = {
+        $set: {
+          image: updatedItem.image,
+          item_name: updatedItem.item_name,
+          subcategory_Name: updatedItem.subcategory_Name,
+          short_description: updatedItem.short_description,
+          price: updatedItem.price,
+          rating: updatedItem.rating,
+          customization: updatedItem.customization,
+          processing_time: updatedItem.processing_time,
+          stockStatus: updatedItem.stockStatus
+        }
+      }
+      const result = await craftCollection.updateOne(filter, SingleItem, options);
       res.send(result)
     })
 
